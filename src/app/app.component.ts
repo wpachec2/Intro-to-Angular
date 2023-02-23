41f3fc735ed7326f59f1a340086a86b10eedd537
wpachec2 <fredojpacheco@gmail.com>
2023-02-23 16:03:42 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Menu';
+
+  allItems: MenuItem[] = [
+    {
+      item: "Chicken Fingers",
+      category: "Dinner",
+      price: 11.99
+    },
+    {
+      item: "Pizza",
+      category: "Dinner",
+      price: 11.99
+    },
+    {
+      item: "Wings",
+      category: "Side",
+      price: 8.99
+    },
+    {
+      item: "Breadsticks",
+      category: "Side",
+      price: 4.99
+    },
+    {
+      item: "Ceaser Salad",
+      category: "Salad",
+      price: 3.99
+    },
+    {
+      item: "Cinnamon Roll",
+      category: "Dessert",
+      price: 7.99
+    }
+  ];
+
+  getByCategory(Cat: string): MenuItem[] {
+    return this.allItems.filter((mi: MenuItem) => 
+      mi.category.includes(Cat)  
+    );
+    console.log(Cat);
+    // return result;
+  }
 }
